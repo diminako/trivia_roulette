069d05f82819176d25463322a3e8899af955e3a2
DimitriNakos <dimi1nakos@gmail.com>
2022-08-20 13:29:03 -0400

    working logix and barebones html/css

--- trivia_logic.js
+++ trivia_logic.js
@@ -3,18 +3,47 @@ fetch("./trivia_locations.json")
     .then(response => {
         return response.json();
     })
-    .then(data => trivia = data.weekdays[0]);
+    .then(data => trivia = data);
 
-const roulette_btn = document.getElementById("roulette_btn")
+const weekday = document.getElementById("weekday");
+const roulette_btn = document.getElementById("roulette_btn");
+const name_target = document.getElementById("name_target");
+const time_target = document.getElementById("time_target");
+const company_target = document.getElementById("company_target");
+const notes_target = document.getElementById("notes_target");
+const rating_target = document.getElementById("rating_target");
+const dayCheckbox = document.getElementsByName("dayCheckbox")
+var days = [];
 
 
-function randomSelection() {
-    let rando = Math.floor(Math.random() * trivia.monday.length);
-    return rando
+const assignValues = (selectedDay, choice, daysValue) => {
+    weekday.innerHTML = daysValue
+    name_target.innerHTML = trivia[selectedDay][choice].name
+    time_target.innerHTML = trivia[selectedDay][choice].time
+    company_target.innerHTML = trivia[selectedDay][choice].company
+    notes_target.innerHTML = trivia[selectedDay][choice].notes
+    rating_target.innerHTML = trivia[selectedDay][choice].rating
 }
 
 roulette_btn.addEventListener("click", function () {
-    console.log(trivia.tuesday[randomSelection()].name)
-    console.log(trivia.tuesday[randomSelection()].time)
-    console.log(trivia.tuesday[randomSelection()].company)
+    days = [];
+    dayCheckbox.forEach(day => {
+        if (day.checked === true) {
+            days.push(day)
+        } else {
+            weekday.innerHTML = "Choose a day, Please.";
+            return
+        }
+    })
+
+    function randomSelection(selectedDay) {
+        return Math.floor(Math.random() * trivia[selectedDay].length);
+    }
+
+    var selectedDay = Math.floor(Math.random() * days.length)
+    var choice = randomSelection(selectedDay);
+    var daysValue = days[selectedDay].id;
+
+    assignValues(selectedDay, choice, daysValue)
 })
+
